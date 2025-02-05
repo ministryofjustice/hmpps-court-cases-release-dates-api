10bb50e5d8b43d55aac6e71535879b7dfb40a2b1
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-02-05 16:41:18 +0000

    CMG-195 set class property

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtcasesreleasedatesapi/model/CacheableThingToDo.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtcasesreleasedatesapi/model/CacheableThingToDo.kt
@@ -1,6 +1,13 @@
 package uk.gov.justice.digital.hmpps.courtcasesreleasedatesapi.model
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
 // wrapping the thing to do in an object for caching null values.
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.MINIMAL_CLASS,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "@class",
+)
 data class CacheableThingToDo(
   val thingToDo: ThingToDo? = null,
 )
