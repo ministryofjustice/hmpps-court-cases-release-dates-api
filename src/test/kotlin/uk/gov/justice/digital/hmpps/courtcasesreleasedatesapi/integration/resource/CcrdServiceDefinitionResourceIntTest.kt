926f79816374f51cb8e4df3dd8e017d0b1e624b0
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-10-03 07:50:18 +0100

    RASS-1369 add court case documents tab (#38)
    * RASS-1369 add court case documents tab
    
    * RASS-1369 add court case documents tab

--- src/test/kotlin/uk/gov/justice/digital/hmpps/courtcasesreleasedatesapi/integration/resource/CcrdServiceDefinitionResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/courtcasesreleasedatesapi/integration/resource/CcrdServiceDefinitionResourceIntTest.kt
@@ -60,6 +60,13 @@ class CcrdServiceDefinitionResourceIntTest : SqsIntegrationTestBase() {
                 "thingsToDo": {
                   "count": 0
                 }
+              },
+              "documents": {
+                "href": "http://localhost:8001/person/AB1234AB/documents",
+                "text": "Documents",
+                "thingsToDo": {
+                  "count": 0
+                }
               }
             }
           }          
